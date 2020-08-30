c9f114bd02e034adbb5a86c0e88b718427575c0f
Takashi Umeno <umeno.takashi@gmail.com>
2020-08-30 12:20:48 +0900

    change: print agentState, lastSeen of thousandeyes-va

--- localtest/getagentlist.py
+++ localtest/getagentlist.py
@@ -23,9 +23,15 @@ response = api_session.request("GET", url, auth=HTTPBasicAuth(thousandeyes_user,
 # If successfully able to get list of flows
 if (response.status_code == 200):
 
-    agentlists = json.loads(response.content)
-    print(json.dumps(agentlists, indent=4))
+    #agents = json.loads(response.content)
+    #print(json.dumps(agentlists, indent=4))
 
+    agents = json.loads(response.content)["agents"]
+    for agent in agents:
+        #print(json.dumps(flow, indent=4)) # formatted print
+        if (agent['agentName'] == 'thousandeyes-va'):
+            print(agent['agentState'])
+            print(agent['lastSeen'])
 # If unable to fetch list of alerts
 else:
     print("An error has ocurred, while fetching agentlists, with the following code {}".format(response.status_code))
