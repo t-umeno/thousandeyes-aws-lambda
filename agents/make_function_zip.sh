3a847e42a86aab3f2672fd22f1760d60c14f6ff3
Takashi Umeno <umeno.takashi@gmail.com>
2020-08-30 16:57:49 +0900

    new file:   make_function_zip.sh

---
+++ agents/make_function_zip.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+rm -r v-env
+virtualenv --python=python3 v-env
+source v-env/bin/activate
+pip install boto3
+pip install requests
+rm function.zip
+(cd $VIRTUAL_ENV/lib/python3.6/site-packages; zip -r9 ${OLDPWD}/function.zip .
+)
+zip -g function.zip get_flows_lambda.py
+deactivate
+rm -r v-env
