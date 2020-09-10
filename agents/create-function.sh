0d9e691a96728a4b1cb3913d1354d60bc29b8dd9
Takashi Umeno <umeno.takashi@gmail.com>
2020-09-10 23:06:18 +0900

    change: ./create-function.sh <AWS_ACCOUNT_ID> <S3_BUCKET>

--- agents/create-function.sh
+++ agents/create-function.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-aws s3 rm s3://thousandeyes-agent/function.zip
-aws s3 cp function.zip s3://thousandeyes-agent/
+AWS_ACCOUNT_ID=$1
+S3_BUCKET=$2
+aws s3 rm s3://${S3_BUCKET}/function.zip
+aws s3 cp function.zip s3://${S3_BUCKET}/
 aws lambda delete-function --function-name AgentStatus
-aws lambda create-function --debug --function-name AgentStatus --runtime python3.6 --role arn:aws:iam::633906190213:role/lambda-s3fullaceess-role --handler agent_status_lambda.lambda_handler --code S3Bucket=thousandeyes-agent,S3Key=function.zip --timeout 30 --memory-size 128
-aws s3 rm s3://thousandeyes-agent/function.zip
+aws lambda create-function --debug --function-name AgentStatus --runtime python3.6 --role arn:aws:iam::${AWS_ACCOUNT_ID}:role/lambda-s3fullaceess-role --handler agent_status_lambda.lambda_handler --code S3Bucket=${S3_BUCKET},S3Key=function.zip --timeout 30 --memory-size 128
+aws s3 rm s3://${S3_BUCKET}/function.zip
